4bff14e92cd672c0c49668eed3a8eecdf8b4ac02
Daewon Lee <17400dxlee@gmail.com>
2019-08-27 19:11:52 -0500

    Started on item form and some functionality

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <button 
+            class="todo-item" 
+            v-bind:class="{'is-complete':todo.completed}" 
+            @click="markCompleted"
+            >{{todo.title}}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markCompleted() {
+            this.todo.completed = !this.todo.completed
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .is-complete {
+        text-decoration: line-through;
+    }
+    .todo-item {
+        background-color: lightgray;
+        border-left: 5px solid green;
+        line-height: 200%;
+        margin-bottom: 10px;
+        width: 100%;
+        padding: 10px;
+        font-size: 15px;
+    }
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
-    <h1>TODO List goes here</h1>
+    <h1>TODO List</h1>
+    <div class="input-form">
+        <input type="Enter Todo">
+        <button>Submit</button>
+    </div>
     <div v-bind:key="todo.id" v-for="todo in todos">
-        <h3>{{todo.title}}</h3>
+        <todo-item v-bind:todo="todo"/>
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue'
+
 export default {
   name: 'TodoList',
+  components: {
+      TodoItem
+  },
   props: ["todos"]
 }
 </script>
 
 <style scoped>
-
+    .input-form {
+        margin-bottom: 10px;
+        width: 100%;
+    }
+    input {
+        /* width: 100% */
+    }
 </style>
